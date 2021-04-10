cf9f90efd4aadaf8dc2e9390f3c2a92de845b411
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-04-10 06:55:19 +0530

    prototypal_inheritance1

--- prototypal_inheritance.js
+++ prototypal_inheritance.js
@@ -1,20 +1,24 @@
-const myObject = {
+const myObject1 = {
   price: 500,
   get_price: function () {
     return this.price
   },
 }
 
-// function myObject() {
-//   this.price = 500
-// }
-
-// myObject.prototype.get_price = function () {
-//   return this.price
-// }
+function myObject() {
+  this.price = 500
+}
+myObject.prototype.get_price = function () {
+  return this.price
+}
 
 // const customObject = Object.create(myObject.prototype)
-const customObject = Object.create(myObject)
+// customeObject constructor would be myObject AND  its __proto__ would contain
+// myObject.prototype properties and methods, Using myObject to create new object
+// would built-in Function as constructor and __proto__ would contain myObject as function
+const customObject = Object.create(myObject1)
+// myObject1 is of type Object, so constructor in this case would be built-in
+// Object type and __proto__ would contain myObject1 object
 customObject.price = 200
 
 delete customObject.price
